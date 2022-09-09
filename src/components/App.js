d045b8b6879afde5af7ff2c5332f92de65e2fbbe
ryandefea <ryan.defea@gmail.com>
2022-09-09 13:57:51 -0500

    remove hard coded info and insert components

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,10 @@ import Header from "./Header";
 import CoffeeControl from "./CoffeeControl";
 
 function App() {
-  const name = "Colombian Peaberry";
   return (
     <React.Fragment>
-      <h1>Coffee Time</h1>
-      <h3>{name}</h3>
-      <h3>origin-Colombia</h3>
-      <p>price-$3.50</p>
-      <p>roast-Dark</p>
-      <hr/>
+      <Header />
+      <CoffeeControl />
     </React.Fragment>
   );
 }

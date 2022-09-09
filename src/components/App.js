f381eac850d62628f3fd0cb83736fb2425db0f3a
ryandefea <ryan.defea@gmail.com>
2022-09-09 11:30:45 -0500

    add hard code coffe info

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import CoffeeControl from "./CoffeeControl";
 
 function App() {
+  const name = "Colombian Peaberry";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1>Coffee Time</h1>
+      <h3>{name}</h3>
+      <h3>origin-Colombia</h3>
+      <p>price-$3.50</p>
+      <p>roast-Dark</p>
+      <hr/>
+    </React.Fragment>
   );
 }
 

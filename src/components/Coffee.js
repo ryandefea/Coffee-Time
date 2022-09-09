adde8b8720daa2a6ed96e75d7b5b3a525ec1443d
ryandefea <ryan.defea@gmail.com>
2022-09-09 18:58:46 -0500

    add in id prop

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -4,10 +4,12 @@ import PropTypes from "prop-types";
 function Coffee(props) {
   return (
     <React.Fragment>
+      <div onClick = {() => props.whenCoffeeClicked(props.id)}>
       <h3>{props.name} - {props.origin}</h3>
       <p>{props.price}</p>
       <p>{props.roast}</p>
       <hr/>
+      </div>
     </React.Fragment>
   );
 }
@@ -16,7 +18,9 @@ Coffee.propTypes = {
   name: PropTypes.string.isRequired,
   origin: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
-  roast: PropTypes.string.isRequired
+  roast: PropTypes.string.isRequired,
+  id: PropTypes.string,
+  whenCoffeeClicked: PropTypes.func
 };
 
 export default Coffee;

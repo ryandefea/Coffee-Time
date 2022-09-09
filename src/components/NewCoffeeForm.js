f9c0990d03cc71e5e567839b40612e738f44e7aa
ryandefea <ryan.defea@gmail.com>
2022-09-09 17:06:54 -0500

    move form info to new coffee form

--- src/components/NewCoffeeForm.js
+++ src/components/NewCoffeeForm.js
@@ -1,11 +1,48 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { v4 } from "uuid";
 
 function NewCoffeeForm(props){
+  function handleNewCoffeeFormSubmission(event) {
+    event.preventDefault();
+    props.onNewCoffeeCreation({
+    name: event.target.name.value,
+    origin: event.target.origin.value,
+    price: event.target.price.value,
+    roast: event.target.roast.value,
+    id: v4()
+  });
+  }
+
+  NewCoffeeForm.propTypes = {
+    onNewCoffeeCreation: PropTypes.func
+  };
+
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit={handleNewCoffeeFormSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Coffee Name' />
+        <input
+          type='text'
+          name='origin'
+          placeholder='Coffee Origin' />
+        <input
+          type='text'
+          name='price'
+          placeholder='Money' />
+        <input
+          type='text'
+          name='roast'
+          placeholder='Coffee Roast' />
+        <button type='submit'>Help!</button>
+      </form>
     </React.Fragment>
   );
 }
+
+
 
 export default NewCoffeeForm;

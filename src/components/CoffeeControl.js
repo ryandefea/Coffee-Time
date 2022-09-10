9972f6c4588f58d8bac61e090d584593a0951fff
ryandefea <ryan.defea@gmail.com>
2022-09-09 19:00:35 -0500

    add in changing selected coffee and delete coffee methods

--- src/components/CoffeeControl.js
+++ src/components/CoffeeControl.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NewCoffeeForm from "./NewCoffeeForm";
 import CoffeeList from "./CoffeeList";
+import CoffeeDetail from "./CoffeeDetail";
 
 class CoffeeControl extends React.Component {
 
@@ -22,20 +23,46 @@ class CoffeeControl extends React.Component {
     });
   }
 
+  handleChangingSelectedCoffee = (id) => {
+    const selectedCoffee = this.state.mainCoffeeList.filter(coffee => coffee.id === id)[0];
+    this.setState({selectedCoffee: selectedCoffee});
+  }
+
+  handleDeletingCoffee = (id) => {
+    const newMainCoffeeList = this.state.mainCoffeeList.filter(ticket => ticket.id !== id);
+    this.setState({
+      mainCoffeeList: newMainCoffeeList,
+      selectedCoffee: null
+    });
+  }
+
   handleClick = () => {
+    if (this.state.selectedCoffee != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedCoffee: null,
+        editing: false
+      });
+    } else {
     this.setState(prevState => ({
       formVisibleOnPage: !prevState.formVisibleOnPage
-    }));
+      }));
+    }
   }
 
   render() {
     let currentlyVisibleState = null;
     let buttonText = null;
-    if (this.state.formVisibleOnPage) {
+
+    if (this.state.selectedCoffee != null) {
+      currentlyVisibleState = <CoffeeDetail coffee = {this.state.selectedCoffee} onClickingDelete = {this.handleDeletingCoffee} />
+      buttonText = "Return to Coffee List";
+    }
+    else if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewCoffeeForm onNewCoffeeCreation={this.handleAddingNewCoffeeToList}/>
       buttonText = "Return to Coffee List";
     } else {
-      currentlyVisibleState = <CoffeeList coffeeList={this.state.mainCoffeeList}/>;
+      currentlyVisibleState = <CoffeeList coffeeList={this.state.mainCoffeeList}onCoffeeSelection={this.handleChangingSelectedCoffee}/>;
       buttonText = "Add Coffee";
     }
     return (

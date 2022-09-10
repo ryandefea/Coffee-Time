1f5e52a04e18c8a071f93fadb8049731fd7ece54
ryandefea <ryan.defea@gmail.com>
2022-09-09 19:03:35 -0500

    ad form info from new coffee form to reusable form

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Coffee Name' />
+        <input
+          type='text'
+          name='origin'
+          placeholder='Coffee Origin' />
+        <input
+          type='text'
+          name='price'
+          placeholder='Money' />
+        <input
+          type='text'
+          name='roast'
+          placeholder='Coffee Roast' />
+        <button type='submit'>Help!</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmitted: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;

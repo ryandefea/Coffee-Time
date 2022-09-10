e42f85d48f98448741d25f0825ffccfcbe6ab3f2
ryandefea <ryan.defea@gmail.com>
2022-09-09 19:02:57 -0500

    remove form info from new coffee form

--- src/components/NewCoffeeForm.js
+++ src/components/NewCoffeeForm.js
@@ -1,48 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { v4 } from "uuid";
+import ReusableForm from "./ReusableForm";
 
 function NewCoffeeForm(props){
   function handleNewCoffeeFormSubmission(event) {
     event.preventDefault();
     props.onNewCoffeeCreation({
-    name: event.target.name.value,
-    origin: event.target.origin.value,
-    price: event.target.price.value,
-    roast: event.target.roast.value,
-    id: v4()
-  });
+      name: event.target.name.value,
+      origin: event.target.origin.value,
+      price: event.target.price.value,
+      roast: event.target.roast.value,
+      id: v4()
+    });
   }
-
-  NewCoffeeForm.propTypes = {
-    onNewCoffeeCreation: PropTypes.func
-  };
-
   return (
     <React.Fragment>
-      <form onSubmit={handleNewCoffeeFormSubmission}>
-        <input
-          type='text'
-          name='name'
-          placeholder='Coffee Name' />
-        <input
-          type='text'
-          name='origin'
-          placeholder='Coffee Origin' />
-        <input
-          type='text'
-          name='price'
-          placeholder='Money' />
-        <input
-          type='text'
-          name='roast'
-          placeholder='Coffee Roast' />
-        <button type='submit'>Help!</button>
-      </form>
+      <ReusableForm
+        formSubmissionHandler={handleNewCoffeeFormSubmission}
+        buttonText="Submit Order" />
     </React.Fragment>
   );
 }
 
 
+NewCoffeeForm.propTypes = {
+  onNewCoffeeCreation: PropTypes.func
+};
 
 export default NewCoffeeForm;

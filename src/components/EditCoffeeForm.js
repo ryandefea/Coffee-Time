7bccee361207ea8c46160c0ecac53ed0908e4fae
ryandefea <ryan.defea@gmail.com>
2022-09-11 12:15:56 -0500

    add in handle edit coffee form submission that capture form values and triggers the handle edit coffee in list  editing coffee method

--- src/components/EditCoffeeForm.js
+++ src/components/EditCoffeeForm.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReusableForm from './ReusableForm';
+import PropTypes from "prop-types";
 
 function EditCoffeeForm (props) {
+  const { coffee } = props;
+
+  function handleEditCoffeeFormSubmission(event) {
+    event.preventDefault();
+    props.onEditCoffee({name: event.target.name.value, origin: event.target.origin.value, price: event.target.price.value, roast: event.target.roast.value, id: coffee.id})
+  }
   return (
     <React.Fragment>
       <ReusableForm
-      buttonText="Update Coffee" />
+        formSubmissionHandler={handleEditCoffeeFormSubmission}
+        buttonText="Update Coffee" />
     </React.Fragment>
   );
 }
+
+EditCoffeeForm.propTypes = {
+  coffee: PropTypes.object,
+  onEditCoffee: PropTypes.func
+};
 
 export default EditCoffeeForm;

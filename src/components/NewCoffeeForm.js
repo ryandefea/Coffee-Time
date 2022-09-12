4f699cff74ee8d96cce491b869194bf4a09148d4
ryandefea <ryan.defea@gmail.com>
2022-09-11 19:08:24 -0500

    add burlap of beans prop to new coffee form

--- src/components/NewCoffeeForm.js
+++ src/components/NewCoffeeForm.js
@@ -11,6 +11,7 @@ function NewCoffeeForm(props){
       origin: event.target.origin.value,
       price: event.target.price.value,
       roast: event.target.roast.value,
+      burlapOfBeans: 130,
       id: v4()
     });
   }
